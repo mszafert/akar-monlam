f03bd9131549d2fb8f4a6ae9045c892dbc6b558a
Marcin Szafert <mszafert@gmail.com>
2024-11-10 23:07:15 +0100

    ADD reset styles and css utility tool

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
 import antfu from '@antfu/eslint-config';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
 export default antfu({
   astro: true,
+  plugins: [jsxA11y],
   stylistic: {
     semi: true,
   },
   typescript: {
     tsconfigPath: 'tsconfig.json',
   },
+  unocss: true,
 });

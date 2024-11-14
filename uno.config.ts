e11f26f4ffcd133df7c008d9624ab96062069875
Marcin Szafert <mszafert@gmail.com>
2024-11-14 23:36:53 +0100

    UPDATE set default theme color

--- uno.config.ts
+++ uno.config.ts
@@ -2,5 +2,10 @@ import { defineConfig, presetUno, transformerDirectives } from 'unocss';
 
 export default defineConfig({
   presets: [presetUno()],
+  theme: {
+    colors: {
+      blue: '#0085fe',
+    },
+  },
   transformers: [transformerDirectives()],
 });

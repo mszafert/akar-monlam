475e68f21e13066eaec0843d106e10cd142c46f1
Marcin Szafert <mszafert@gmail.com>
2024-12-20 23:57:31 +0100

    UPDATE move script to menu conponnet

--- src/scripts/menu.ts
+++
@@ -1,9 +0,0 @@
-document.querySelector('.menu-nav')?.addEventListener('click', () => {
-  document.querySelector('.menu-links')?.classList.toggle('expanded');
-});
-
-document.addEventListener('click', (e) => {
-  if (!document.querySelector('nav')?.contains(e.target as Node)) {
-    document.querySelector('.menu-links')?.classList.remove('expanded');
-  }
-});

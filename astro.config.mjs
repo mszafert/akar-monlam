8000b553c9bdde326cd3bf3d28c5778cff40e2ec
Marcin Szafert <mszafert@gmail.com>
2025-01-18 00:00:14 +0100

    UPDATE target dist dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './public_nodejs',
   integrations: [UnoCSS({ injectReset: true })],
 });

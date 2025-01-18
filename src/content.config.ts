69eb66535beca9b8ca51e9854b1a35b8c06277b6
Marcin Szafert <mszafert@gmail.com>
2025-01-18 19:37:40 +0100

    ADD image to collections schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,9 +4,12 @@ import { defineCollection, z } from 'astro:content';
 export const collections = {
   teachings: defineCollection({
     loader: glob({ pattern: '**/[^_]*.md', base: './src/content/teachings' }),
-    schema: z.object({
+    schema: ({ image }) => z.object({
       date: z.date(),
       type: z.ZodEnum.create(['upcoming', 'ongoing', 'past']),
+      image: image().optional(),
+      imageCaption: z.string().optional(),
+      imageAlt: z.string().optional(),
       title: z.array(z.string()),
     }),
   }),

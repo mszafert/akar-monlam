c91fb760255cea99dbbd0015a22172ee89f067e0
Marcin Szafert <mszafert@gmail.com>
2025-01-23 21:59:24 +0100

    ADD site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://akar.dotestow.net/',
   trailingSlash: 'always',
   integrations: [UnoCSS({ injectReset: true })],
 });
